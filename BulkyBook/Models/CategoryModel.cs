ebe17355412d4a94d8c87ad19d0a052bec364fd4
lou <dustyfingers01257@gmail.com>
2022-02-13 16:21:38 -0700

    create category model

---
+++ BulkyBook/Models/CategoryModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBook.Models
+{
+    public class CategoryModel
+    {
+
+        // this data annotation tells identity framwork that this field is the primary key for this table
+        [Key]
+        public int Id { get; set; }
+
+        // tell identity framework that Name is required
+        [Required]
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+        // default to current date time
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+
+    }
+}
